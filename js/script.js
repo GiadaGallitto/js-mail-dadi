cf70dc4063a18f88b98bbe0d8cf4d645b25af920
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-24 15:13:16 +0100

    Create structure and outline the game of dices

---
+++ js/script.js
@@ -0,0 +1,16 @@
+GIOCO DEI DADI
+
+-Devo generare un numero randomico per il computer
+-Devo generare un numero randomico per il  giocatore tramite un "button"
+
+Queste generazioni di numeri randomici devono avvenire nel momento in cui l'utente giocatore preme il pulsante
+(quindi all'interno di un addEvenListener)
+
+Quando cliccherà il pulsante, mostrerò il numero random generato per il computer e quello random per il giocatore
+
+Sempre all'interno dell'evento, verranno inseriti due if, in cui:
+
+-se il valore del numero del computer è maggiore di quello del giocatore, apparirà un messaggio in cui dico che ho vinto io;
+
+-se il valore del numero del computer è minore di quello del giocatore, apparirà un messaggio in cui dico che ha vinto lui;
+
