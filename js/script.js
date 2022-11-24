cc79dbad94a3a26c566b7a962929dc3afe31513a
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-24 16:59:06 +0100

    Implement mail validator

--- js/script.js
+++ js/script.js
@@ -52,4 +52,29 @@ buttonGame.addEventListener("click", function(){
 
 let emailList = [
     "Pippo.thePidgeon@gmail.com", "Loki.GodofAsgard@hotmail.it", "Vold.è.Mort@libero.it", "Chisei.Gokunonlosai@sayan.com"
-]
+];
+
+const buttonValidator = document.getElementById("pulsante");
+
+buttonValidator.addEventListener(`click`, function(){
+
+    const email = document.getElementById("email").value;
+
+    let founded;
+
+    for(let i=0; i < emailList.length; i++) {
+
+        if (email == emailList[i]){
+            founded = true;
+        }
+    }
+
+    if (founded){
+        console.log (`email trovata`)
+        document.getElementById("output-mail").innerHTML = "Il tuo indirizzo email è corretto!"
+    } else {
+        console.log (`email non trovata`)
+        document.getElementById("output-mail").innerHTML = "Il tuo indirizzo email non è presente nella nostra lista"
+    }
+
+})
